b56c4ab08b93127772cc81498bd2fe68f7cf65b0
Amit Kanderi <kanderi.amit2@gmail.com>
2018-07-11 01:37:21 +0530

    Max Product Pair problem done

---
+++ workspace/Day 8/maxprod.py
@@ -0,0 +1,27 @@
+# Product to find the max product pair in an array.
+
+
+def maxProduct(arr, n):
+ 
+    if (n < 2):
+        print("No pairs exists")
+        return
+     
+    # Initialize max product pair
+    a = arr[0]; b = arr[1]
+ 
+    # Traverse through every possible pair
+    # and keep track of max product
+    for i in range(0, n):
+         
+        for j in range(i + 1, n):
+            if (arr[i] * arr[j] > a * b):
+                a = arr[i]; b = arr[j]
+ 
+    print("Max product pair is {", a, ",", b, "}",
+                                          sep = "")
+     
+# Driver Code
+arr = [1, 4, 13, 6, 9, 0]
+n = len(arr)
+maxProduct(arr, n)
