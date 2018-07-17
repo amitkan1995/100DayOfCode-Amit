35b9539fafc4925dd4437510f4c1f6469c4b322a
Amit Kanderi <kanderi.amit2@gmail.com>
2018-07-17 23:42:16 +0530

    Problem Solved

---
+++ workspace/Day 15/count_alpha.py
@@ -0,0 +1,14 @@
+# To count the frequecny of alphabet in a string
+
+def count(st):
+    occur={}
+    for i in st:
+        kys=occur.keys()
+        if i in kys:
+            occur[i]+=1
+        else:
+            occur[i]=1
+    return occur
+
+
+print(count('Biscuits.com'))
