22040d6e635a0f2e7723110f9c45497f911fa4bf
Amit Kanderi <kanderi.amit2@gmail.com>
2018-07-19 00:16:45 +0530

    Writing TO CSV Done

--- workspace/Day 16/datatocsv.py
+++ workspace/Day 16/datatocsv.py
@@ -5,24 +5,39 @@ import csv
 
 def detailsinput():
     details={}
-    details['name']=input("Enter name: ")
+    details['Name']=input("Enter name: ")
        
     details['Reg no']=int(input("Enter Registration No. : "))
 
-    marks={}
-    marks['Physics']=int(input("Enter Physics marks: "))
-    marks['Chemistry']=int(input("Enter Chemistry marks: "))
-    marks['Maths']=int(input("Enter Maths marks: "))
-    marks['Percentage']=int(input("Enter Total Percentage: "))
-    details['Marks']=marks
-    details['course']=input("Enter course: ")
-
-
-
-with open('details.csv', 'w') as csvfile:
-    fieldnames = []
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
- 
-    writer.writeheader()
     
-print("Writing complete")
+    details['Physics']=int(input("Enter Physics marks: "))
+    details['Chemistry']=int(input("Enter Chemistry marks: "))
+    details['Maths']=int(input("Enter Maths marks: "))
+    details['Percentage']=int(input("Enter Total Percentage: "))
+    
+    details['Course']=input("Enter course: ")
+    details['Status']=input("Enter status of student: ")
+    
+
+    return  details
+
+def writeToCsv(detail):
+    with open('details.csv', 'w') as csvfile:
+        fieldnames = ['Name','Reg no','Course','Physics','Chemistry','Maths','Percentage','Status']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow(detail)
+    print("Writing complete")
+    
+#def readFromCsc():
+
+
+
+print("1. For New entry:")
+print("2. Edit details of any student")
+print("3. Display Details of Students")
+ch=int(input())
+det={}
+if ch==1:
+    det=detailsinput()
+    writeToCsv(det)
