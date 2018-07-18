522d88af05e7c63396c65fdaffdfc0f7d6512142
Amit Kanderi <kanderi.amit2@gmail.com>
2018-07-18 22:44:59 +0530

    CSV input done

--- workspace/Day 16/datatocsv.py
+++ workspace/Day 16/datatocsv.py
@@ -2,3 +2,27 @@
 
 #import necessery libraries
 import csv
+
+def detailsinput():
+    details={}
+    details['name']=input("Enter name: ")
+       
+    details['Reg no']=int(input("Enter Registration No. : "))
+
+    marks={}
+    marks['Physics']=int(input("Enter Physics marks: "))
+    marks['Chemistry']=int(input("Enter Chemistry marks: "))
+    marks['Maths']=int(input("Enter Maths marks: "))
+    marks['Percentage']=int(input("Enter Total Percentage: "))
+    details['Marks']=marks
+    details['course']=input("Enter course: ")
+
+
+
+with open('details.csv', 'w') as csvfile:
+    fieldnames = []
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+ 
+    writer.writeheader()
+    
+print("Writing complete")
