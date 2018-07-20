4382491af401428cc0f8a5fbfab1b35f169ffacb
Amit Kanderi <kanderi.amit2@gmail.com>
2018-07-20 23:50:31 +0530

    Problem done

--- workspace/Day 16/datatocsv.py
+++ workspace/Day 16/datatocsv.py
@@ -6,10 +6,7 @@ import csv
 def detailsinput():
     details={}
     details['Name']=input("Enter name: ")
-       
     details['Reg no']=int(input("Enter Registration No. : "))
-
-    
     details['Physics']=int(input("Enter Physics marks: "))
     details['Chemistry']=int(input("Enter Chemistry marks: "))
     details['Maths']=int(input("Enter Maths marks: "))
@@ -29,15 +26,41 @@ def writeToCsv(detail):
         writer.writerow(detail)
     print("Writing complete")
     
-#def readFromCsc():
+def readFromCsv():
+
+    with open('names.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row['Name'], row['Reg no'], row['Course'], row['Physics'], row['Maths'], row['Percentage'], row['Status'])
+
+
+def editdetails():
+    print("Enter Updated details:")
+    deta=detailsinput()
+    for row in reader:
+        if name == row['name']:
+            with open('details.csv', 'w') as csvfile:
+                writer.writerow({'name': row['name'], 'number': row['number'], 'address': row['address']})
+                writer.writerow(detail)
+    print("Writing complete")
+
+
 
 
 
 print("1. For New entry:")
 print("2. Edit details of any student")
 print("3. Display Details of Students")
+print("4. Change Status of Student")
 ch=int(input())
 det={}
 if ch==1:
     det=detailsinput()
     writeToCsv(det)
+elif ch==2:
+    
+elif ch==3:
+    readFromCsv()  
+elif ch==4:
+
+    
