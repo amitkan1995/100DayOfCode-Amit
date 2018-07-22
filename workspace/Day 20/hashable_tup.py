04b525237af85da11002c917e93ff34f2e75e74b
Amit Kanderi <kanderi.amit2@gmail.com>
2018-07-22 22:48:21 +0530

    Tuple Problem done

--- workspace/Day 20/hashable_tup.py
+++ workspace/Day 20/hashable_tup.py
@@ -3,5 +3,5 @@
 if __name__ == '__main__':
     n = int(input())
     integer_list = map(int, input().split())
-    t=tuple(integer_list)
-    print(hash(t))
+    t=tuple(integer_list) # We need to create a tuple as lists are not hashable
+    print(hash(t)) # Print the hash value of the tuple
