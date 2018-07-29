a645a2da3462f8d9949875a1d9bd9adf6ff3b113
Amit Kanderi <kanderi.amit2@gmail.com>
2018-07-29 23:35:46 +0530

    Test my javascript knowledge

---
+++ workspace/Day 27/testknowledge.js
@@ -0,0 +1,8 @@
+console.log(3+true) // 4  (3 + value of true i.e 1)
+console.log(true+false) // 1 (value of true + value of false)
+console.log('4'+8) // 48 concatenation of 4 and 8
+console.log(true+'4')  // true4 concatenation of true and 4
+console.log(1+1+'1') // 21 Addition of 
+console.log('1'+1+1) // 111 Concatenation
+console.log(-'69'+69) // 0
+console.log(-'kietgroupofinstitution' + '429') // Not a Number
