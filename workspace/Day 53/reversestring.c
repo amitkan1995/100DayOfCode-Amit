8f2931cf31fc9d0045c54bfdff4d42f6f393e96f
Amit Kanderi <kanderi.amit2@gmail.com>
2018-08-24 21:29:01 +0530

    Reverse String Array Wise

--- workspace/Day 53/reversestring.c
+++ workspace/Day 53/reversestring.c
@@ -41,7 +41,7 @@ void reverse(char *begin, char *end)
 /* Driver function to test above functions */
 int main()
 {
-  char s[] = "i like this program very much";
+  char s[] = "i like coding very much";
   char *temp = s;
   reverseWords(s);
   printf("%s", s);
