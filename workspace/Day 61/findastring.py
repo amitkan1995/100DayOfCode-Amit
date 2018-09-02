f1adbdc1ac44ec471d9c9d8ab8b8b313ffc3f8f8
Amit Kanderi <kanderi.amit2@gmail.com>
2018-09-02 23:04:15 +0530

    FInd a string Hacekerrank Problem

---
+++ workspace/Day 61/findastring.py
@@ -0,0 +1,20 @@
+# Find a string on Hackerrank
+def count_substring(string, sub_string):
+    sublen=len(sub_string)
+    #print(sublen,":",len(string))
+    count=0
+    for i in range(0,len(string)):
+        temp=""
+        if(len(string)-i<sublen):
+            break  
+        t=string[i:sublen+i]
+        if(sub_string==t):
+            count=count+1
+    return count
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
