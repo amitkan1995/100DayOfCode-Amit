6df97c82050e0c1410aee00cfe52938b8d05bb94
Amit Kanderi <kanderi.amit2@gmail.com>
2018-09-06 23:43:17 +0530

    Deletion linked List on python

---
+++ workspace/Day 65/deletionlinkedlist.py
@@ -0,0 +1,54 @@
+# Deletion Linked List
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    
+    def push(self,new_data):
+        new_node=Node(new_data)
+        new_node.next=self.head
+        self.head=new_node
+    
+    def deleteNode(self,key):
+        temp=self.head
+        # Check if it is head node or not
+        if(temp is not None):
+            if(temp.data==key):
+                self.head=temp.next
+                temp=None
+                return
+        
+        while(temp is not None):
+            if temp.data==key:
+                break
+            prev=temp
+            temp=temp.next
+
+        if(temp==None):
+            return
+        
+        prev.next=temp.next
+        temp=None
+
+    def printList(self):
+        temp=self.head
+        while temp:
+            print(temp.data)
+            temp=temp.next
+    
+llist=LinkedList()
+llist.push(8)
+llist.push(1)
+llist.push(4)
+llist.push(2)
+
+print("Linked List created")
+llist.printList()
+llist.deleteNode(4)
+print("After deletion of 4")
+llist.printList()
