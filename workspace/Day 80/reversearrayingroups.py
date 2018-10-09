ea071104ebacad858f1ad5204e374809abd5993c
Amit Kanderi <kanderi.amit2@gmail.com>
2018-10-09 20:37:51 +0530

    Array reverse in group problem

--- workspace/Day 80/reversearrayingroups.py
+++ workspace/Day 80/reversearrayingroups.py
@@ -21,7 +21,7 @@ def reverse(arr, n, k):
         while (left < right): 
               
             arr[left], arr[right] = arr[right], arr[left] 
-            left+= 1; 
+            left+= 1
             right-+1
         i+= k 
       
