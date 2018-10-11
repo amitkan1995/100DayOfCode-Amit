ebdff3d1b8bd1312700c696c679ac57edac77bb0
Amit Kanderi <kanderi.amit2@gmail.com>
2018-10-11 21:54:26 +0530

    Hackerrank Linked list Problem

---
+++ workspace/Day 82/hackerrank.py
@@ -0,0 +1,34 @@
+# Hackerrank Problem
+
+mport math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def printLinkedList(head):
+    temp= head
+    while temp:
+        print(temp.data)
+        temp=temp.next
