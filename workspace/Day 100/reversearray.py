221ee331e8e5deda55782ea56909cc7be1139881
Amit Kanderi <kanderi.amit2@gmail.com>
2018-10-30 22:21:59 +0530

    Reverse array in Blocks

--- workspace/Day 100/reversearray.py
+++ workspace/Day 100/reversearray.py
@@ -17,9 +17,9 @@ def reverseArray( arr, start, end):
 # of size n by d  
 def rightRotate( arr, d, n): 
       
-    reverseArray(arr, 0, n - 1); 
-    reverseArray(arr, 0, d - 1); 
-    reverseArray(arr, d, n - 1); 
+    reverseArray(arr, 0, n - 1)
+    reverseArray(arr, 0, d - 1) 
+    reverseArray(arr, d, n - 1) 
   
   
 # function to pr an array  
